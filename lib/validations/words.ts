afb146a23b1f1ee1c60103eb05a1ddf624b6fecf
Lahiru Yasas <lahiruyasas7@gmail.com>
2026-08-02 23:53:02 +0530

    feat: create validation schema file words and set array validation example sentence

---
+++ lib/validations/words.ts
@@ -0,0 +1,37 @@
+import { PartOfSpeech } from "@/app/generated/prisma/enums";
+import { z } from "zod";
+
+export const exampleSentenceSchema = z
+  .string()
+  .min(1)
+  .max(500, "Sentence must be less than 500 characters");
+
+export const wordSchema = z.object({
+  text: z
+    .string()
+    .trim()
+    .trim()
+    .min(1, "Word is required")
+    .max(100, "Word must be less than 100 characters"),
+  meaning: z
+    .string()
+    .trim()
+    .trim()
+    .min(1, "Meaning is required")
+    .max(1000, "Meaning must be less than 1000 characters"),
+  partOfSpeech: z.nativeEnum(PartOfSpeech, {
+    message: "Please select a part of speech",
+  }),
+  notes: z
+    .string()
+    .trim()
+    .max(500, "Notes must be less than 500 characters")
+    .optional(),
+  exampleSentences: z
+    .array(exampleSentenceSchema)
+    .max(10, "Maximum 10 example sentences allowed")
+    .optional(),
+  categoryIds: z.array(z.string()).optional(),
+});
+
+export type WordInput = z.infer<typeof wordSchema>;
