a8b877afbd59d635333e10d12b624b7aa8a365c8
Lahiru Yasas <lahiruyasas7@gmail.com>
2026-08-12 19:18:55 +0530

    chore: remove database url and auth secret for now.

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,11 +4,6 @@ import path from "path";
 export default defineConfig({
   test: {
     environment: "node",
-    env: {
-      DATABASE_URL: process.env.DATABAE_URL!,
-      AUTH_SECRET: "Srgf5KhbBAQqc4YtvDYisLT7EwGXLMQL",
-    },
-    setupFiles: ["./lib/test-setup.ts"],
   },
   resolve: {
     alias: {
