7dd70ad8e170e73b9673f844e3eda81e92ab06ff
Lahiru Yasas <lahiruyasas7@gmail.com>
2026-08-15 00:01:11 +0530

    feat: set default route as /words

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/words",
+        permanent: true, // Uses a 308 permanent redirect for SEO
+      },
+    ];
+  },
 };
 
 export default nextConfig;
