d9f6cc0f72fdddc5ab5b71f00a587cb99df95eaa
Lahiru Yasas <lahiruyasas7@gmail.com>
2026-08-17 14:34:20 +0530

    refactor: remove types for part of speech data in file and import from words.type.ts file

--- app/components/word-detail-modal.tsx
+++ app/components/word-detail-modal.tsx
@@ -5,28 +5,7 @@ import Link from "next/link";
 import { useTransition } from "react";
 import { useRouter } from "next/navigation";
 import { deleteWord } from "@/lib/actions/words";
-
-const POS_STYLES: Record<string, string> = {
-  NOUN: "bg-blue-50 text-blue-700",
-  VERB: "bg-green-50 text-green-700",
-  ADJECTIVE: "bg-purple-50 text-purple-700",
-  ADVERB: "bg-orange-50 text-orange-700",
-  PRONOUN: "bg-pink-50 text-pink-700",
-  PREPOSITION: "bg-yellow-50 text-yellow-700",
-  CONJUNCTION: "bg-red-50 text-red-700",
-  INTERJECTION: "bg-teal-50 text-teal-700",
-};
-
-const POS_LABELS: Record<string, string> = {
-  NOUN: "noun",
-  VERB: "verb",
-  ADJECTIVE: "adj",
-  ADVERB: "adv",
-  PRONOUN: "pron",
-  PREPOSITION: "prep",
-  CONJUNCTION: "conj",
-  INTERJECTION: "interj",
-};
+import { POS_LABELS, POS_STYLES } from "../types/words.type";
 
 interface WordDetailModalProps {
   word: {
